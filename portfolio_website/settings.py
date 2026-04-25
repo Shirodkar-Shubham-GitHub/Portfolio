951fd25c359f55ac8edbb3040e0fb57a60c315d7
Shubham Ashok Shirodkar <shubhamshirodkar90@gmail.com>
2026-04-25 15:39:53 +0530

    Changes in settings.py for staticfiles

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -181,8 +181,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",   # where your style.css is
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  # for collectstatic
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
